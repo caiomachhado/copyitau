f113c810d46de1185619bde00cae96bbffa2a600
Caio Machado <caioamsul@gmail.com>
2021-04-07 16:33:06 -0300

    first commit

---
+++ app/src/main/java/com/example/clone_ita/CanvasView.kt
@@ -0,0 +1,21 @@
+package com.example.clone_ita
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class CanvasView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        val paint: Paint()
+        canvas.drawLine(0f, )
+
+    }
+
+}
